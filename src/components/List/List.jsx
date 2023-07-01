0efec2c003be1126a29c7b9c8d9d652484c8bc32
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-01 21:34:48 +0530

    List structure done

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 import useStyles from './styles';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 const List = () => {
   const classes = useStyles();
   const [type, setType] = useState('restaurants');
   const [rating, setRating] = useState('');
+  const places =[
+    {name:'Cool Place'},
+    {name:'Best Beer'},
+    {name:'Best Chicken'},
+    {name:'Cool Place'},
+    {name:'Best Beer'},
+    {name:'Best Chicken'}
+  ];
 
   return (
     <div className={classes.container}>
@@ -30,6 +39,14 @@ const List = () => {
           <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
+
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place}/>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
