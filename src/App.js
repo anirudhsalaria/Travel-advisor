dfca983597dd5748847cbabff46988fd9ea005a3
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-02 14:23:46 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { CssBaseline,Grid } from '@material-ui/core';
+import { getPlacesData } from './api';
+import { useEffect, useState  } from 'react';
 
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
+  const [places,setPlaces] = useState([]);
+
+  useEffect(() => {
+    getPlacesData()
+        .then((data) => {
+            console.log(data);
+            setPlaces(data);
+        })
+  },[]);
+
   return (
     <>
       <CssBaseline/>
