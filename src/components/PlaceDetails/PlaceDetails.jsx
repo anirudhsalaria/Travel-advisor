f333e9e7108d3811bee1de39717259c2fc11c4be
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-02 21:17:48 +0530

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -6,6 +6,7 @@ import { Rating } from '@material-ui/lab/Rating';
 import useStyles from './styles';
 
 const PlaceDetails = ({place}) => {
+  const classes = useStyles();
   console.log(place)
   return (
     <Card elevation={6}>
@@ -26,6 +27,34 @@ const PlaceDetails = ({place}) => {
           <Typography variant='subtitle1'>Ranking</Typography>
           <Typography gutterBottom variant='subtitle1'>{place.ranking}</Typography>
         </Box>
+        {place?.awards?.map((award) => (
+          <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+            <img src={award.images.small} alt={award.display_name}/>
+            <Typography variant='subtitle2' color='textSecondaey'>{award.display_name}</Typography>
+          </Box>
+        ))}
+        {place?.cuisine?.map(({name}) => (
+            <Chip key={name} size='small' label={name} className={classes.chip}/>
+        ))}
+        {place?.address && (
+          <Typography gutterBottom variant='subtitle2' color='textSecondary' className={classes.subtitle}>
+            <LocationOnIcon/> {place.address}
+          </Typography>
+        )}
+        {place?.phone && (
+          <Typography gutterBottom variant='subtitle2' color='textSecondary' className={classes.spacing}>
+            <PhoneIcon/> {place.phone}
+          </Typography>
+        )}
+        <CardActions>
+          <Button size='small' color='primary' onClick={() => window.open(place.web_url,'_blank')}>
+            Trip Advisor
+          </Button>
+          <Button size='small' color='primary' onClick={() => window.open(place.website,'_blank')}>
+            Website
+          </Button>
+        </CardActions>
+
       </CardContent>
     </Card>
   )
