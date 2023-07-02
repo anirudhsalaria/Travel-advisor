36497f2f6215cb33bc6075abaac6b0d8b654d8ba
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-02 14:51:02 +0530

    making map dynamic

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ import Map from './components/Map/Map';
 
 const App = () => {
   const [places,setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
+      navigator.geolocation.getCurrentPosition(({ coords: {latitude,longitude}})=> {
+          setCoordinates({lat:latitude, lng: longitude})
+      })
+  },[]);
+
+  useEffect(() => {
+    console.log(coordinates,bounds);
     getPlacesData()
         .then((data) => {
             console.log(data);
             setPlaces(data);
         })
-  },[]);
+  },[coordinates, bounds]);
 
   return (
     <>
@@ -27,7 +36,11 @@ const App = () => {
           <List/>
         </Grid>
         <Grid item xs={12} md={4}>
-          <Map/>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,10 +6,9 @@ import { Rating } from '@material-ui/lab';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({setCoordinates, setBounds, coordinates}) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
-  const coordinates = { lat:0, lng:0};
 
   return (
     
@@ -21,7 +20,10 @@ const Map = () => {
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
+        onChange={(e) => {
+          setCoordinates({lat: e.center.lat, lng: e.center.lng});
+          setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+        }}
         onChildClick={''}
       >
 
